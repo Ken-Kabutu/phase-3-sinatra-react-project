dc5634494090dc6d65fd2b5b04b0701f9242610e
Ken <kabutukenneth3@gmail.com>
2023-07-24 09:45:45 +0300

    setting up routes in user.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,17 @@ class UsersController < ApplicationController
         end
     end
 
+     # PUT /users/:id
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            user.to_json
+        else
+            status 422
+            { error: "Failed to update user" }.to_json
+        end
+    end
+
       # DELETE /users/:id
     def destroy
         user = User.find(params[:id])
