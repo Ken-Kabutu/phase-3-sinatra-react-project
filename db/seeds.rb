8741576b17d803aa3b5310bc767f246656ad545e
Ken <kabutukenneth3@gmail.com>
2023-07-24 08:10:14 +0300

    creating user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+require './db/config' # Load the database configuration file
+require './models/user' # Load the User model
+
+# Seed data for users
+5.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password' # Replace with appropriate password hashing in production
+  )
+end
 
 puts "✅ Done seeding!"
