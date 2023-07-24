6cd2c0715e2238ccf7bc0a5f5ca894c2cb2eafba
Ken <kabutukenneth3@gmail.com>
2023-07-24 09:24:19 +0300

    adding GET to  memberships controller file

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,3 +1,9 @@
 class MembershipsController < ApplicationController
+      # GET /memberships
+    def index
+        memberships = Membership.all
+        render json: memberships
+    end
+
     
 end
