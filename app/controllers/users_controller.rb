440fa53f778fa44fc45731656b8af9295c4a18a3
Ken <kabutukenneth3@gmail.com>
2023-07-24 10:26:06 +0300

    updating the user controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     # GET /users
     def index
         users = User.all
-        users.to_json
+        render json: users
     end
 
    # GET /users/:id
