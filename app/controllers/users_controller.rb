fa761d0c1b7a49f341413f4534c1a87456c7e3e0
Ken <kabutukenneth3@gmail.com>
2023-07-24 09:08:13 +0300

    adding POST in user controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,13 @@ class UsersController < ApplicationController
         render json: user
     end
 
-    
+    #POST/users
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: { error: 'Failed to create user.' }, status: :unprocessable_entity
+        end
+    end
 end
