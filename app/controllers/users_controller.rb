a7149fe4f8eb36a7f1120cf0a6c73fdd251e9c72
Ken <kabutukenneth3@gmail.com>
2023-07-24 09:06:17 +0300

    adding GET_id in user controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-     # GET /users
-  def index
-    users = User.all
-    render json: users
-  end
+    # GET /users
 
-  
+    def index
+        users = User.all
+        render json: users
+    end
+
+   # GET /users/:id
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    
 end
